5da882de6a3cb10d5e987b732c01634a158e67e3
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-18 18:20:01 +0700

    feat: average page

--- app/routes/average-price-calculation.tsx
+++ app/routes/average-price-calculation.tsx
@@ -2,67 +2,169 @@ import {
   Container,
   Box,
   Heading,
-  Image,
-  Input,
-  InputLeftAddon,
-  InputGroup,
+  NumberInput,
+  NumberInputField,
+  Grid,
+  FormControl,
+  FormLabel,
   Card,
   CardHeader,
   CardBody,
   Text,
-  Grid,
+  RadioGroup,
+  Radio,
+  Stack,
 } from "@chakra-ui/react";
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionArgs, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
 
 import getMeta from "~/helpers/getMeta";
 
-export const meta: MetaFunction = () =>
-  getMeta({ title: "Average Price Calculation" });
+const BUY_TYPE = "1";
+const SELL_TYPE = "2";
 
-export default function AveragePriceCalculation() {
+export const meta: MetaFunction = () =>
+  getMeta({ title: "Average Calculation" });
+
+export default function AverageCalculation() {
+  const actionData = useActionData<typeof action>();
+
+  const { averagePrice = 0, stockLot = 0, amount = 0 } = actionData || {};
+
   return (
     <Container maxW="container.lg" pt={8} pb={16}>
-      <Box marginBottom={12}>
-        <Heading as="h2" size="xl" marginBottom={4}>
-          Average Price Calculation
-        </Heading>
-        <Box marginBottom={6}>
-          <InputGroup>
-            <InputLeftAddon children="Rp" />
-            <Input placeholder="Put stock price here" type="number" />
-          </InputGroup>
+      <Form method="post">
+        <Box marginBottom={12}>
+          <Heading as="h2" size="xl" marginBottom={4}>
+            Average Calculation
+          </Heading>
+          <Box marginBottom={6}>
+            <FormControl marginBottom={6}>
+              <FormLabel>Transaction Type</FormLabel>
+              <RadioGroup defaultValue={BUY_TYPE}>
+                <Stack spacing={5} direction="row">
+                  <Radio colorScheme="blue" value={BUY_TYPE}>
+                    Buy
+                  </Radio>
+                  <Radio colorScheme="red" value={SELL_TYPE}>
+                    Sell
+                  </Radio>
+                </Stack>
+              </RadioGroup>
+            </FormControl>
+            <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+              <FormControl>
+                <FormLabel>Current Lot</FormLabel>
+                <NumberInput>
+                  <NumberInputField
+                    placeholder="Put stock lot here"
+                    name="currentLot"
+                  />
+                </NumberInput>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Current Price</FormLabel>
+                <NumberInput>
+                  <NumberInputField
+                    placeholder="Put stock price here"
+                    name="currentPrice"
+                  />
+                </NumberInput>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Target Lot</FormLabel>
+                <NumberInput>
+                  <NumberInputField
+                    placeholder="Put stock lot here"
+                    name="targetLot"
+                  />
+                </NumberInput>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Target Price</FormLabel>
+                <NumberInput>
+                  <NumberInputField
+                    placeholder="Put stock price here"
+                    name="targetPrice"
+                  />
+                </NumberInput>
+              </FormControl>
+            </Grid>
+          </Box>
+          <button type="submit" hidden>
+            Create Todo
+          </button>
         </Box>
+      </Form>
+      <Box>
+        <Heading as="h3" size="lg" marginBottom={4}>
+          Result
+        </Heading>
         <Box display="flex">
-          <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+          <Grid templateColumns="repeat(3, 1fr)" gap={6} w="100%">
             <Card>
               <CardHeader>
-                <Heading size="md">Auto Rejection Bawah (ARB)</Heading>
+                <Heading size="md">Average Price</Heading>
               </CardHeader>
               <CardBody>
-                <Text fontSize="6xl" fontWeight="bold" color="red.500">
-                  10000 (7%)
+                <Text fontSize="4xl" fontWeight="bold" color="green.500">
+                  {averagePrice}
                 </Text>
               </CardBody>
             </Card>
             <Card>
               <CardHeader>
-                <Heading size="md">Auto Rejection Atas (ARA)</Heading>
+                <Heading size="md">Lot</Heading>
               </CardHeader>
               <CardBody>
-                <Text fontSize="6xl" fontWeight="bold" color="green.400">
-                  10000 (35%)
+                <Text fontSize="4xl" fontWeight="bold" color="green.500">
+                  {stockLot}
+                </Text>
+              </CardBody>
+            </Card>
+            <Card>
+              <CardHeader>
+                <Heading size="md">Amount</Heading>
+              </CardHeader>
+              <CardBody>
+                <Text fontSize="4xl" fontWeight="bold" color="green.500">
+                  {amount}
                 </Text>
               </CardBody>
             </Card>
           </Grid>
         </Box>
       </Box>
-      <Box>
-        <Heading as="h2" size="lg" marginBottom={4}>
-          IDX Information
-        </Heading>
-        <Image src="/imgs/ara-arb.jpg" alt="ARA ARB" maxW="560px" />
-      </Box>
     </Container>
   );
 }
+
+export const action = async ({ request }: ActionArgs) => {
+  const body = await request.formData();
+  const currentLot = Number(body.get("currentLot"));
+  const currentPrice = Number(body.get("currentPrice"));
+  const targetLot = Number(body.get("targetLot"));
+  const targetPrice = Number(body.get("targetPrice"));
+
+  if (
+    typeof currentLot !== "number" ||
+    typeof currentPrice !== "number" ||
+    typeof targetLot !== "number" ||
+    typeof targetPrice !== "number"
+  ) {
+    throw new Error(`Form not submitted correctly.`);
+  }
+
+  const currentAmount = currentLot * currentPrice;
+  const targetAmount = targetLot * targetPrice;
+
+  const averagePrice =
+    (currentAmount + targetAmount) / (currentLot + targetLot);
+
+  return json({
+    averagePrice,
+    stockLot: currentLot + targetLot,
+    amount: currentAmount + targetAmount,
+  });
+};
